15f5a1a84e5f8995a047695641ae4a1ac12d84e9
Amandella <matthewdipillo@gmail.com>
2024-04-14 11:06:15 -0400

    Added enemy spawner

--- Scripts/enemyspawner.gd
+++ Scripts/enemyspawner.gd
@@ -2,8 +2,11 @@ extends Node3D
 
 const ENEMY = preload("res://Scenes/enemy.tscn")
 
+@onready var spawn_timer = $SpawnTimer
+
 var spawnPositions: Array
 var enemy: Enemy
+var skipSpawn: bool = false
 
 func _ready():
 	spawnPositions = get_children()
@@ -11,5 +14,18 @@ func _ready():
 	
 func spawn_enemies():
 	for positions in spawnPositions:
+		randomizePositions()
+		if skipSpawn:
+			skipSpawn = false
+		else:
+			positions.add_child(enemy)
+
+func randomizePositions():
+	var randomSpawns = randi_range(1,10)
+	if randomSpawns >= 7:
 		enemy = ENEMY.instantiate()
-		positions.add_child(enemy)
+	else:
+		skipSpawn = true
+
+func _on_spawn_timer_timeout():
+	spawn_enemies()
