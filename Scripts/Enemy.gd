ea95e2369a976e61fcf6aa4f614db44e99f7a66a
Vincent <vjc1988@gmail.com>
2024-04-14 11:05:52 -0400

    Added lava slug

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,6 +4,8 @@ class_name Enemy
 
 @export var SPEED: int = 2
 
+@onready var animation_player = $lavaslug/AnimationPlayer
+
 var direction: Vector3
 var player_to_attack:CharacterBody3D = null
 
@@ -15,10 +17,15 @@ func _physics_process(_delta):
 
 	if !player_to_attack == null:
 		direction = global_position.direction_to(Vector3(player_to_attack.global_position.x,0,player_to_attack.global_position.z))
+		look_at(Globals.currentPlayer.global_position)
 	elif player_to_attack == null:
 		if Globals.currentPlayer != null:
 			player_to_attack = Globals.currentPlayer
 	
+	
 	velocity = SPEED * direction
+
+	if velocity.x or velocity.z:
+		animation_player.play("ArmatureAction")
 
 	move_and_slide()
