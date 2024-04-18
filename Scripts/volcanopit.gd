b2fd336fb4d6ddf5e3b8b9106ebb23b00699b872
Vincent <vjc1988@gmail.com>
2024-04-17 21:45:08 -0400

    Added a lava shader

---
+++ Scripts/volcanopit.gd
@@ -0,0 +1,11 @@
+extends Node3D
+#
+#
+#@onready var mesh_instance_3d = $MeshInstance3D
+#
+#var offset = Vector3.ZERO
+#var speed = Vector3(50.0,1150.0,115.0)
+#
+#func _process(delta):
+	#offset += speed*delta
+	#mesh_instance_3d.get_surface_override_material(0).set_shader_parameter("shader_parameter/noise1/noise/offset", offset)
